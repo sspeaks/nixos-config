e78ad536963eaea1306b592a7885022b1f6c6dc4
Seth Speaks <sspeaks610@gmail.com>
2021-04-18 23:35:32 -0700

    added option

--- post-compinit.zsh
+++ post-compinit.zsh
@@ -8,3 +8,7 @@ zstyle ':completion:*' special-dirs true
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,comm'
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+
+if [ -f ~/.nix-profile/etc/profile.d/nix.sh ]; then
+  source ~/.nix-profile/etc/profile.d/nix.sh
+fi
