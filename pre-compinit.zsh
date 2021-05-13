c12dccbf6131841b81036039ddffcdfe22ef5b54
Seth Speaks <sspeaks610@gmail.com>
2021-05-13 13:45:49 -0700

    Changed shell prompt and added aliases

--- pre-compinit.zsh
+++ pre-compinit.zsh
@@ -1,4 +1,4 @@
-PROMPT='$(shell-prompt "$?" "${__shadowenv_data%%:*}" "${__dev_source_dir}")'
+#PROMPT='$(shell-prompt "$?" "${__shadowenv_data%%:*}" "${__dev_source_dir}")'
 setopt prompt_subst
 setopt NO_BEEP
 setopt PROMPT_SP
--- shell-prompt/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/burke/dotfiles/dotfiles-src/shell-prompt
-
-go 1.13
