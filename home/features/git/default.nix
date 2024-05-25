9d82ab736570a017dd2f964a977c90a47de17a45
Seth Speaks <sspeaks610@gmail.com>
2024-05-25 13:57:56 -0700

    update git

--- home/features/git/default.nix
+++ home/features/git/default.nix
@@ -17,6 +17,11 @@
       current = "log --decorate --stat -1";
       lasthash = "log -1 --pretty=format:'%h'";
     };
+    extraConfig = {
+      credential = {
+        helper = "store";
+      };
+    };
   };
 
 }
