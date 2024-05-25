2f08a89ae0f736841ea008314c99a6bc8ab31ccf
Seth Speaks <sspeaks610@gmail.com>
2024-05-25 12:55:24 -0700

    make nixpi manage home-manager

--- hosts/nixpi/default.nix
+++ hosts/nixpi/default.nix
@@ -1,21 +1,26 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
+let
+
+in
 {
   imports = [
     ../common/global
     ../common/users/sspeaks
     ./hardware-config.nix
-
+    inputs.home-manager.nixosModules.home-manager
     inputs.vscode-server.nixosModules.default
-    ({ config, pkgs, ... }: {
-      services.vscode-server.enable = true;
-    })
   ];
 
   networking = {
     hostName = "nixpi";
   };
 
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.sspeaks = ../../home/sspeaks.nix;
+
+  services.vscode-server.enable = true;
   security.sudo.wheelNeedsPassword = false;
 
   time.timeZone = "America/Los_Angeles";
