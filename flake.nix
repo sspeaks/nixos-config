3edcdb1776a01af074d3792b9142ac58c4eb438c
Seth Speaks <sspeaks610@gmail.com>
2024-05-25 13:32:07 -0700

    add hardware config and update pkgs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-  description = "NixOS RPi4 Config";
+  description = "NixOS Config";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
-nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     systems.url = "github:nix-systems/default-linux";
   };
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, nixos-wsl, systems, ... }:
