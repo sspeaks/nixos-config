beb0514d928fdefc2dceda8e0aadf4814679189d
Seth Speaks <sspeaks610@gmail.com>
2024-05-25 12:57:48 -0700

    add nixosWSL host

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
-
+nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     systems.url = "github:nix-systems/default-linux";
   };
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, systems, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, nixos-wsl, systems, ... }:
     let
       inherit (self) outputs;
       lib = nixpkgs.lib;
@@ -30,10 +30,20 @@
             hosts/nixpi
           ];
         };
+        NixOS-WSL = lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            hosts/nixosWSL
+          ];
+        };
       };
       homeConfigurations = {
         "sspeaks@nixpi" = home-manager.lib.homeManagerConfiguration {
           pkgs = pkgsFor.aarch64-linux;
+          modules = [ home/sspeaks.nix ];
+        };
+        "sspeaks@NixOS-WSL" = home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgsFor.x86_64-linux;
           modules = [ home/sspeaks.nix ];
         };
       };
