f1e100a53563bcdfd6ff0a721847fbf3cfcb6f7d
sspeaks610 <marialith191@gmail.com>
2024-05-25 20:13:42 -0700

    added sops

---
+++ hosts/common/sops.nix
@@ -0,0 +1,13 @@
+{ inputs, config, ...}:
+{
+  imports = [ inputs.sops-nix.nixosModules.sops ];
+  sops = {
+    defaultSopsFile = ../../secrets.yaml;
+  age = {
+    sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    keyFile = "/var/lib/sops-nix/key.txt";
+    generateKey = true;
+  };
+
+  };
+}
