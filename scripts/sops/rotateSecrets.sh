a55d13e532ab707760df05f87fe40cc602f3d3ff
Seth Speaks <sspeaks610@gmail.com>
2024-05-28 12:04:20 -0700

    rotated keys and added a sops rotate script

---
+++ scripts/sops/rotateSecrets.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p sops ssh-to-age
+
+DEFAULT_SECRETS="secrets.yaml"
+SECRETS_FILE="${1:-$DEFAULT_SECRETS}"
+
+SOPS_AGE_KEY="$(sudo cat /etc/ssh/ssh_host_ed25519_key | ssh-to-age -private-key)" sops -r -i "$SECRETS_FILE"
