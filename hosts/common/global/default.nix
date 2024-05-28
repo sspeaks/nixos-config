7105ae47ec0b2283140242b7c37dff1946148529
Seth Speaks <sspeaks610@gmail.com>
2024-05-27 18:43:40 -0700

    remove x11 default

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -16,7 +16,7 @@
   };
 
   services.openssh.enable = lib.mkDefault true;
-  services.openssh.settings.X11Forwarding = lib.mkDefault true;
+  services.openssh.settings.X11Forwarding = lib.mkDefault false;
 
   hardware.enableRedistributableFirmware = true;
 
