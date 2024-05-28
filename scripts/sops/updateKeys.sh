247de5d48e5b18017c08ee7811940530aab87cd3
Seth Speaks <sspeaks610@gmail.com>
2024-05-28 11:42:08 -0700

    add new host

--- scripts/sops/updateKeys.sh
+++ scripts/sops/updateKeys.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p sops ssh-to-age
-SOPS_AGE_KEY="$(sudo cat /etc/ssh/ssh_host_ed25519_key | ssh-to-age -private-key)" sops updatekeys secrets.yaml
+
+DEFAULT_SECRETS="secrets.yaml"
+SECRETS_FILE="${1:-$DEFAULT_SECRETS}"
+
+SOPS_AGE_KEY="$(sudo cat /etc/ssh/ssh_host_ed25519_key | ssh-to-age -private-key)" sops updatekeys "$SECRETS_FILE"
