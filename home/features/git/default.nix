2c63684c81c0f0cf97353ebc4e5cbee52c2ffa9e
Seth Speaks <sspeaks610@gmail.com>
2024-05-28 18:43:27 -0700

    add private ssh key

--- home/features/git/default.nix
+++ home/features/git/default.nix
@@ -21,6 +21,9 @@
       credential = {
         helper = "store";
       };
+      core = {
+        sshCommand = "ssh -i ~/.ssh/github";
+      };
     };
   };
 
--- hosts/common/users/sspeaks/default.nix
+++ hosts/common/users/sspeaks/default.nix
@@ -2,6 +2,13 @@
 {
   sops.secrets.sspeaks-password.neededForUsers = true;
 
+  sops.secrets.github-ssh-private = {
+    path = "/home/sspeaks/.ssh/github";
+    owner = "sspeaks";
+    group = "users";
+    mode = "0600";
+  };
+
   programs.zsh.enable = true;
   users.mutableUsers = false;
   users.users.sspeaks = {
