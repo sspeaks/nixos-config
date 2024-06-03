aa6db452cc12d74cafd3e1da45be6b20da3ccf4a
Seth Speaks <sspeaks610@gmail.com>
2024-06-03 16:49:25 -0700

    add telescope plugin

--- home/features/neovim/default.nix
+++ home/features/neovim/default.nix
@@ -1,4 +1,16 @@
 { pkgs, inputs, ... }:
+let
+  telescope_live_args = pkgs.vimUtils.buildVimPlugin {
+    name = "telescope-live-grep-args";
+    src = pkgs.fetchFromGitHub {
+      owner = "nvim-telescope";
+      repo = "telescope-live-grep-args.nvim";
+      rev = "4122e146d199c0d6d1cfb359c76bc1250d522460";
+      sha256 = "sha256-a5IaLd7q9vRJmfiXux7xvXg6vPNouV2+ShdqY/vbHnw=";
+    };
+  };
+in
+
 {
   imports = [ inputs.nixvim.homeManagerModules.nixvim ];
 
@@ -16,10 +28,12 @@
         keymaps = {
           "<leader>ff" = "find_files";
           "<leader>gf" = "git_files";
-          "<leader>fg" = "live_grep";
+          "<leader>fg" = "live_grep_args";
+          "<leader>b" = "buffers";
         };
+        enabledExtensions = [ "live_grep_args" ];
       };
     };
-    extraPlugins = with pkgs.vimPlugins; [ ale vim-nix ];
+    extraPlugins = with pkgs.vimPlugins; [ ale vim-nix telescope_live_args ];
   };
 }
