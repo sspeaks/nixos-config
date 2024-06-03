958f9728d818edd0e445a51a3addf7e6d5d5a905
Seth Speaks <sspeaks610@gmail.com>
2024-06-03 14:40:13 -0700

    add telescope

--- modules/udp2rawServer.nix
+++ modules/udp2rawServer.nix
@@ -6,7 +6,7 @@ in
   systemd.services.ptunnelserver = {
     description = "pTunnel Server";
     serviceConfig = {
-      ExecStart = "${ptunnelPackage}/bin/udp2raw -s -l 0.0.0.0:25565 -r 127.0.0.1:51820 -k \"P|pitone12\" --raw-mode icmp -a --log-level 5";
+      ExecStart = "${ptunnelPackage}/bin/udp2raw -s -l 0.0.0.0:25565 -r 127.0.0.1:51820 -k \"password\" --raw-mode icmp -a --log-level 5";
       Restart = "always";
       RestartSec = 1;
     };
