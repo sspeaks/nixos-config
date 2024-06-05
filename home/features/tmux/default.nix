d69ede57830fe6441f187e0c68c83187c666be01
Seth Speaks <sspeaks610@gmail.com>
2024-06-05 13:14:21 -0700

    adjust tmux

--- home/features/tmux/default.nix
+++ home/features/tmux/default.nix
@@ -4,13 +4,37 @@
     enable = true;
     keyMode = "vi";
     prefix = "C-s";
-    mouse = true;
     extraConfig = ''
       bind-key h select-pane -L
       bind-key j select-pane -D
       bind-key k select-pane -U
       bind-key l select-pane -R
-    '';
-    plugins =  with pkgs; [ tmuxPlugins.cpu tmuxPlugins.catppuccin ];
+          '';
+    plugins =  with pkgs; [ 
+      tmuxPlugins.cpu 
+      { 
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_window_number_position "right"
+
+          set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W"
+
+          set -g @catppuccin_status_modules_right "directory session"
+          set -g @catppuccin_status_left_separator  " "
+          set -g @catppuccin_status_right_separator ""
+          set -g @catppuccin_status_fill "icon"
+          set -g @catppuccin_status_connect_separator "no"
+
+          set -g @catppuccin_directory_text "#{pane_current_path}"      
+        '';
+      }
+    ];
   };
 }
