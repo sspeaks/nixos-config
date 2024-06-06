bb05e8bb4f8299ddaa6c3842af27f1edc5e5b8d6
Seth Speaks <sspeaks610@gmail.com>
2024-06-05 22:57:17 -0700

    add simc

--- packages/default.nix
+++ packages/default.nix
@@ -2,4 +2,5 @@
   askGPT4 = pkgs.callPackage ./askGPT4/default.nix { };
   ptunn = pkgs.callPackage ./ptunn/default.nix { };
   udp2raw = pkgs.callPackage ./udp2raw/default.nix { };
+  simc = pkgs.callPackage ./simc/default.nix { };
 }
