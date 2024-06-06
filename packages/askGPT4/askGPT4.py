11e9e39f4e6b6ee4d56cd684dc76a7415a572917
Seth Speaks <sspeaks610@gmail.com>
2024-06-06 16:13:46 -0700

    update chatGPT model

--- packages/askGPT4/askGPT4.py
+++ packages/askGPT4/askGPT4.py
@@ -15,7 +15,7 @@ client = OpenAI(
 )
 
 response = client.chat.completions.create(
-  model="gpt-4-turbo",
+  model="gpt-4o",
   messages=[
     {"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": f"{sys.argv[1]}"}
