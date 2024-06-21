e02d65649d39f068c87bd3c0cb636f5564fca6ac
sspeaks610 <marialith191@gmail.com>
2024-06-20 22:48:17 -0700

    refactor askgpt4 to prepare for context

--- packages/askGPT4/askGPT4.py
+++ packages/askGPT4/askGPT4.py
@@ -3,54 +3,62 @@ import sys
 import os
 import magic
 import base64
+from openai import OpenAI
 
-if len(sys.argv) == 1:
-  print ("No args given, please paste a text to ask a question")
-  exit()
 
-def user_message(text, imgBuff):
-    message = {
-        "role": "user",
-        "content": [ { "type": "text", "text": text } ]
-    }
+model = "gpt-4o"
 
-    if imgBuff is not None:
-        b64 =  base64.b64encode(imgBuff).decode('utf-8')
-        message["content"].append({
+class GPTSession:
+    def __init__(self, api_key, initial_message=None):
+        self.client = OpenAI(
+            api_key=api_key.strip(),
+        )
+    
+    def send_message(self, txt, image = None, context = None):
+        messages = []
+        userMessage = {
+            "role": "user",
+            "content": [ { "type": "text", "text": txt } ]
+        }
+
+        if image is not None:
+            b64 =  base64.b64encode(image).decode('utf-8')
+            userMessage["content"].append({
                 "type": "image_url",
                 "image_url": {
                         "url": f"data:image/png;base64,{b64}"
                     }
-            })
-    return message
-                
+                })
+
+        if context is None:
+            systemMessage = {"role": "system", "content": "You are a helpful assistant."}
+            messages = [ systemMessage, userMessage ]
+        response = self.client.chat.completions.create(
+            model=model,
+            messages=messages
+        )
+        return response.choices[0].message.content
 
 def determineFileType(buffer):
     return magic.from_buffer(b)
 
-image = None
-try:
-    if not sys.stdin.isatty():
-        b = sys.stdin.buffer.read()
-        if 'image' in determineFileType(b):
-            image = b
-except Exception as e:
-    print('Error reading image from stdin:', e)
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+      print ("No args given, please paste a text to ask a question")
+      exit()
+    
+    OPEN_AI_API = open(os.getenv("OPEN_AI_KEY"), "r").read()
 
-OPEN_AI_API = open(os.getenv("OPEN_AI_KEY"), "r").read()
-from openai import OpenAI
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=OPEN_AI_API.strip(),
-)
+    image = None
+    try:
+        if not sys.stdin.isatty():
+            b = sys.stdin.buffer.read()
+            if 'image' in determineFileType(b):
+                image = b
+    except Exception as e:
+        print('Error reading image from stdin:', e)
 
-response = client.chat.completions.create(
-  model="gpt-4o",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    user_message(sys.argv[1], image)
-  ]
-)
-txt_response = response.choices[0].message.content
-print(txt_response)
+    sess = GPTSession(OPEN_AI_API)
+    txt_response = sess.send_message(sys.argv[1], image)
+    print(txt_response)
