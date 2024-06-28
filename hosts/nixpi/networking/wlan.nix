5384394040eab9d882337e998fa3eeddf4f87f23
Seth Speaks <sspeaks610@gmail.com>
2024-06-28 13:24:25 -0700

    add toggle wireguard script and change wifi password

--- hosts/nixpi/networking/wlan.nix
+++ hosts/nixpi/networking/wlan.nix
@@ -8,12 +8,15 @@
         pskRaw = "074a0c1d175390072ecdbe9ad918d4459d1e376b2c9da370057938cb203b734c";
       };
       "hide your kids hide your wifi" = {
-        pskRaw = "";
+        pskRaw = "d67fd930108289665f064b2621f8b19248e5cad01e620148f5830818058de08a";
       };
       "Starbucks WiFi" = {
         auth = ''
           key_mgmt=NONE
         '';
+      };
+      "Picioccio" = {
+        pskRaw = "b2c7b74b2989b9017b42e95e174deaf74a285f780f79d4a09f7fe8d5625c4858";
       };
     };
   };
