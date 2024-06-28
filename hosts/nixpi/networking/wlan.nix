fba7903bd885e8ebff3b033db55ea0a33ec5a13c
Seth Speaks <sspeaks610@gmail.com>
2024-06-28 13:27:19 -0700

    remove another key

--- hosts/nixpi/networking/wlan.nix
+++ hosts/nixpi/networking/wlan.nix
@@ -5,10 +5,10 @@
     interfaces = [ "wlan0" ];
     networks = {
       "SethPhone" = {
-        pskRaw = "";
+        pskRaw = "074a0c1d175390072ecdbe9ad918d4459d1e376b2c9da370057938cb203b734c";
       };
       "hide your kids hide your wifi" = {
-        pskRaw = "d67fd930108289665f064b2621f8b19248e5cad01e620148f5830818058de08a";
+        pskRaw = "";
       };
       "Starbucks WiFi" = {
         auth = ''
