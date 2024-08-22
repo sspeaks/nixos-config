7aee9c12c58e9186788a843f71093a46a543b2fd
Seth Speaks <sspeaks610@gmail.com>
2024-08-21 21:47:18 -0700

    remove password

--- hosts/nixpi/networking/wlan.nix
+++ hosts/nixpi/networking/wlan.nix
@@ -8,7 +8,7 @@
         pskRaw = "074a0c1d175390072ecdbe9ad918d4459d1e376b2c9da370057938cb203b734c";
       };
       "hide your kids hide your wifi" = {
-        pskRaw = "d67fd930108289665f064b2621f8b19248e5cad01e620148f5830818058de08a";
+        pskRaw = "";
       };
       "Starbucks WiFi" = {
         auth = ''
