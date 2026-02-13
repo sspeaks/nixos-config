043323d890840576099130f70de2adaf210d8b92
Seth Speaks <sspeaks610@gmail.com>
2026-02-12 19:08:13 -0800

    update copilot

--- packages/github-copilot-cli.nix
+++ packages/github-copilot-cli.nix
@@ -1,8 +1,8 @@
 { pkgs, fetchzip }:
 pkgs.github-copilot-cli.overrideAttrs (_: rec {
-  version = "0.0.407-1";
+  version = "0.0.409";
   src = fetchzip {
     url = "https://registry.npmjs.org/@github/copilot/-/copilot-${version}.tgz";
-    hash = "sha256-h3XBvWHwS6lbHMjCwvUPZiIo9Ss6pMHtTSR5h03S4Ls=";
+    hash = "sha256-JcnZesLHH1LFtAE91Dzx0t4cGYj/j3ifDtSrAcvaw0s=";
   };
 })
