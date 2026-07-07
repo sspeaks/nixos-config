9e4fb2890e262f961630f9ca4ac06e9bf262cb5c
sspeaks610 <marialith191@gmail.com>
2026-07-07 12:31:09 -0700

    fix nixos-azure base?

--- hosts/nixos-azure/default.nix
+++ hosts/nixos-azure/default.nix
@@ -9,6 +9,11 @@
     inherit inputs outputs;
   };
 
+  nixpkgs = {
+    overlays = lib.mkDefault outputs.lib.overlayList;
+    config.allowUnfree = lib.mkDefault true;
+  };
+
   services.openssh = {
     enable = true;
     settings = {
@@ -23,6 +28,7 @@
   ];
 
   programs.zsh.enable = lib.mkDefault true;
+  programs.nix-ld.enable = lib.mkDefault true;
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
@@ -44,6 +50,7 @@
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwUcujpjx453KzHN6KqLUXZ6OOtfAIhVLe/1c82fm/Tpjh1g93CHw+UlbQJPb/R6+bbXlyWF1kjUGJEeVX9F048VNltUJashsmQtzdxmB2WHyOzBEaVTuEgKnZJXr353Yajg8vo6t0q/n9ZyTh9X+oHW1X2yPCDi0RzQ6yGUYJ/5WiShKzgLWPzn/PdzsvlFLlk8tyOkA7cGxhQTryWKjx7Lc3G5FfZ1hEj9r29R135FXEZkYaEmWVYSgURhUBwC1Klvsp8Bj/rXAd5CVyXSKjWRNw8OaqKr72Dl2J9hoC/XgOgGlL29Xb4c6HVGZW3a/udlnLwyw/C4ynqrABSw+Pt5+1NNLirKbCQfgEOyXtJOD/dJLcqSXYDk/Xj79D8SH0Zl+PV1diDqtVX13JXj1kYM00anEwqLM+e0PnFq+dtHdudJOEHIYDW5iqeHoxTO/WFvaetMaS67UKV5H9c4P+8kFws///sE49aL0m0+plYCRLwQFT+TfImFc/9xYgqUU="
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDC78YTLful5gTbzdbO15Rx7XePaGtxj3nyt1QVrS9/BnlJDeE1J1ah+H4Fh0WvIN6HfgjU54EFTZ07/5A/qmVJWRAnBWBxzMaQNFMKrOZaApTjZWxFPQMa49dMPQ+m5qFpLc2RBqJNE35OBJONDeXYgtlh+ym1BGIenQsxpQ67vsV0vBXtpIi9VjUVj+1Ni0p4s6tUQL5+PBTItKtJt+yhpEA8fThLjgVJC44sCaS7R6SB9gmdLHeaKp9ZS7KTHB+rwvZl4xiek9Hel4/fHjGgbM/aWSR0HzNsFaO/0+vRORHyF4GLtaQZlqoEziuOsDtsBfBfRcbeIqCBCxXV5MHViC70ZQZClpsLReXE2xmwYcWFrWly1pWvgl3WiDGkB4mqCf/pTVGI5jk7HViv2WFUBak1UjJAigHNPIEgS3z9Hd/Qzvcycuo4P5ZV49aWdj4GTHS38ekhMU5JS1zlQFUyasDhgDwqmT/dOmQ959GmN8vDesWp+ljx+3V3jhyrGNE="
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+ZOxlbIpvXB6NDFUqX2OGGbEyIfA+7Zd6mxq78e5abuYD80bMyJdAS1H/05oBKFI5zV45Bb39DXV9HHxVJXVKQ35bs3sfrf6myTK94grgHbCn3o0pru+PsdtXBnCjsC8EMS9pua17ZPyLgCy1jYxGocCoYpxZoP1CLV+LkHauL2IxXAvZkU+W7pHgphF1jnUNjEl52TY++W5BfEJ6xvCUKj7xDMyXpAmNNdpohFpL2ughbdkL5F8s7O/RQFfzh7O13hWlbdgLHMOcoA3tuLSd5pTZjHvqEs0n1CLT/SnvONtD9uNUMGdLGisMydRVFYmOOJ9LxF1pdEvowExbMvAEa0a7nFLASnmnxqzL1lbFxvUQ5p55s3CO4y1B72lpIRRAwuvOUBrpHw83zq6FQ8Z0C2bbmJa/YFOPre6GPw6WbnvUhsYegvxBEHFPUf5zFBqzZGfjbneRptexq8Yl7vtbXP3jyRMj59IumRBOAKHXQj/6fxo4n3WnkiXGmaWh2pk="
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0XL/A/LJGYwiVh/WZaE8cmSBvPJU878PDTRaPV4ixuz5mT9E2Y+hlrP9eQm4SvznjD8TqaSwAgNlE1BfOFlBZ5UjRlOdDfSSSM9MjxLda+TTwFntRum+3irjFLwAzP1O4HCtavxdvJPpZWdVuR6Ku8WH+9Ls30Kp0SzouGkHVSD2udEQm6yFWfSYfMNEfFzg04SRovLkz3NQpEo8evgbxiNYT7pa0m4RMd7VohJn8H/P7Fl7xeEEJdLNKLPEnyxTK0ZH+hPnoNtPqLp+oz8xqefGtvl8ff9cPvXnz2jIS3b6PR+MGEV6eQIOtKuEDCIx3b0kdoSWY9OeglB9eoAl7mUKKZGpH6pKgCLf7QZUL3QSh3jUxp/jZD2TxdKXd0ejBz4DC9CNIvuu95sDnWmwnch+lJU4ObtXW44Xlfal+SYDSD88GYqwFwPhPakFTRhlncoOh2FL7TUcaiopUhYRl8bg+H1yfC0oiciUrT9HC4Jxp0xR/KLwfymFccWObr1c= sspeaks@Seths-MacBook-Pro.local"
     ];
   };
 
--- overlays.nix
+++ overlays.nix
@@ -1,16 +1,16 @@
 [
   (final: prev: {
     waagent = prev.waagent.overrideAttrs (f: p: {
-      runtimeDeps = [ prev.pkgs.which prev.pkgs.python3 prev.pkgs.gawk prev.pkgs.openssl prev.pkgs.gnupg prev.pkgs.lsof ];
+      runtimeDeps = [ final.pkgs.which final.pkgs.python3 final.pkgs.gawk final.pkgs.openssl final.pkgs.gnupg final.pkgs.lsof ];
       fixupPhase = ''
         mkdir -p $out/bin/
         WAAGENT=$(find $out -name waagent | grep sbin)
         cp $WAAGENT $out/bin/waagent
         wrapProgram "$out/bin/waagent" \
            --prefix PYTHONPATH : $PYTHONPATH \
-           --prefix PATH : "${prev.lib.makeBinPath f.runtimeDeps}" \
-           --set NIX_LD_LIBRARY_PATH ${prev.lib.makeLibraryPath  [prev.stdenv.cc.cc ]} \
-           --set NIX_LD ${builtins.readFile "${prev.stdenv.cc}/nix-support/dynamic-linker"}
+           --prefix PATH : "${final.lib.makeBinPath f.runtimeDeps}" \
+           --set NIX_LD_LIBRARY_PATH ${final.lib.makeLibraryPath  [final.stdenv.cc.cc ]} \
+           --set NIX_LD ${builtins.readFile "${final.stdenv.cc}/nix-support/dynamic-linker"}
         patchShebangs --build "$out/bin/"
       '';
     });
